525a43bdb259e4b223503a1ece5d6bc07263f0b6
Ryan Liu <ryan@launchlab.com.au>
2020-01-17 12:21:50 +1100

    Update landing page

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const FeatureGrid = ({ gridItems }) => (
@@ -17,7 +17,7 @@ const FeatureGrid = ({ gridItems }) => (
               <PreviewCompatibleImage imageInfo={item} />
             </div>
           </div>
-          <p>{item.text}</p>
+          <p className='has-text-centered'>{item.text}</p>
         </section>
       </div>
     ))}
